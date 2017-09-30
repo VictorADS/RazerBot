b7ca4f3594559352cf2cf3f925696a271aeb8208
Unknown <victor.ads75@gmail.com>
2017-09-30 11:24:36 +0200

    mdr

--- src/main/java/Article4.java
+++ src/main/java/Article4.java
@@ -5,7 +5,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class Article4 {
-
+ 
 	public static void main(String[] args) {
 
 		SpringApplication.run(
